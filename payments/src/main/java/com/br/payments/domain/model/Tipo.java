bf13edb0efea2aa0d7e7a0a03c6951e5f0819e7e
Deivy Spaulonci <42875800+deivy-spaulonci@users.noreply.github.com>
2024-05-21 22:34:45 -0300

    segond commit

---
+++ payments/src/main/java/com/br/payments/domain/model/Tipo.java
@@ -0,0 +1,10 @@
+package com.br.payments.domain.model;
+
+import jakarta.persistence.MappedSuperclass;
+
+import java.io.Serializable;
+
+@MappedSuperclass
+public class Tipo implements Serializable{
+
+}
